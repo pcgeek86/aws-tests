8c933820408ebc60431ce25d2b9bd0fbaa1e80a4
Trevor Sullivan <trevor@trevorsullivan.net>
2021-12-28 22:56:58 -0700

    Update aws_s3_tests.ps1

--- aws_s3_tests.ps1
+++ aws_s3_tests.ps1
@@ -3,7 +3,17 @@ $global:BucketName = (New-Guid).Guid
 Describe 'Amazon S3 Tests' {
 
     It "Should successfully create an S3 Bucket" {
-        { New-S3Bucket -BucketName $BucketName; Start-Sleep -Seconds 3 } | Should -Not -Throw
+        { New-S3Bucket -BucketName $BucketName; Start-Sleep -Seconds 2 } | Should -Not -Throw
+    }
+    It 'Should upload an object to S3 Bucket' {
+        { 
+          (New-Guid).Guid | Set-Content -Path tempfile.txt
+          Write-S3Object -BucketName $BucketName -File tempfile.txt
+          Remove-Item -Path tempfile.txt
+        } | Should -Not -Throw
+    }
+    It 'Should delete an object from an Amazon S3 Bucket' {
+        { Remove-S3Object -BucketName $BucketName -Key tempfile.txt -Force } | Should -Not -Throw
     }
     It "Should successfully delete an S3 Bucket" {
         { Remove-S3Bucket -BucketName $BucketName -DeleteBucketContent -Force } | Should -Not -Throw
