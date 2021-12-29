31eefd0e0ea77be8a544d9a178dab51eb48bb479
Trevor Sullivan <trevor@trevorsullivan.net>
2021-12-28 22:48:40 -0700

    Update aws_s3_tests.ps1

--- aws_s3_tests.ps1
+++ aws_s3_tests.ps1
@@ -3,9 +3,9 @@ $global:BucketName = (New-Guid).Guid
 Describe 'Amazon S3 Tests' {
 
     It "Should successfully create an S3 Bucket" {
-        { New-S3Bucket -BucketName $BucketName } | Should -Not -Throw
+        { New-S3Bucket -BucketName $BucketName; Start-Sleep -Seconds 3 } | Should -Not -Throw
     }
     It "Should successfully delete an S3 Bucket" {
-        { Remove-S3Bucket -BucketName $BucketName } | Should -Not -Throw
+        { Remove-S3Bucket -BucketName $BucketName -DeleteBucketContent -Force } | Should -Not -Throw
     }
 }
