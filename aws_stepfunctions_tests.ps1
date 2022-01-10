d038762213763067253023f5c5a37969e47bae90
Trevor Sullivan <trevor@trevorsullivan.net>
2022-01-09 18:19:25 -0700

    Update aws_stepfunctions_tests.ps1

--- aws_stepfunctions_tests.ps1
+++ aws_stepfunctions_tests.ps1
@@ -1,14 +1,16 @@
-$ErrorActionPreference = 'stop'
-$RoleName = (New-Guid).Guid
-$StepFunctionName = (New-Guid).Guid
-
 Describe 'AWS Step Functions Tests' {
+  BeforeAll {
+    $ErrorActionPreference = 'stop'
+    $RoleName = (New-Guid).Guid
+    $StepFunctionName = (New-Guid).Guid
+  }
+
   It 'Should create an AWS IAM Role for AWS Step Functions' {
     {
       $Params = @{
         RoleName = $RoleName
         AssumeRolePolicyDocument = @'
-          {
+{
             "Version": "2012-10-17",
             "Statement": [
               {
@@ -26,18 +28,45 @@ Describe 'AWS Step Functions Tests' {
       New-IAMRole @Params
       $SFNDefaultPolicy = Get-IAMPolicyList | ? PolicyName -eq AWSLambdaRole
       Register-IAMRolePolicy -RoleName $RoleName -PolicyArn $SFNDefaultPolicy[0].Arn
-    }
+    } | Should -Not -Throw
   }
 
-  It 'Should create a Step Functions state machine' {
-    { New-SFNStateMachine -Definition '{ }' -Name (New-Guid).Guid -RoleArn  } | Should -Not -Throw
+  It 'Should throw an exception when creating an invalid Step Functions state machine' {
+    { 
+      $IAMRole = Get-IAMRole $RoleName
+      New-SFNStateMachine -Definition '{ }' -Name $StepFunctionName -RoleArn $IAMRole.Arn } | Should -Throw
+  }
+
+  It 'Should successfully create a Step Functions state machine' {
+    { 
+      $IAMRole = Get-IAMRole $RoleName
+      $StateMachine = @'
+{
+  "StartAt": "Step1",
+  "States": {
+    "Step1": {
+      "Type": "Pass",
+      "End": true
+    }
+  }
+}
+'@
+      New-SFNStateMachine -Definition $StateMachine -Name $StepFunctionName -RoleArn $IAMRole.Arn } | Should -Not -Throw
   }
 
   It 'Should delete a Step Functions state machine' {
-    { Remove-SFNStateMachine -StateMachineArn -Force  } | Should -Not -Throw
+    { 
+      $StepFunction = Get-SFNStateMachineList | ? Name -eq $StepFunctionName
+      Remove-SFNStateMachine -StateMachineArn $StepFunction[0].StateMachineArn -Force  } | Should -Not -Throw
   }
 
   It 'Should delete the Step Functions AWS IAM role' {
-    { Remove-IAMRole -RoleName $RoleName -Force } | Should -Not -Throw
+    { 
+      $AttachedPolicyList = Get-IAMAttachedRolePolicyList -RoleName $RoleName
+      foreach ($AttachedPolicy in $AttachedPolicyList) {
+        Unregister-IAMRolePolicy -RoleName $RoleName -PolicyArn $AttachedPolicy.PolicyArn
+      }
+      Remove-IAMRole -RoleName $RoleName -Force
+    } | Should -Not -Throw
   }
 }
